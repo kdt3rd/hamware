7bdba9737dd17e215a3db9dc15b772804e7bf386
Kimball Thurston <kdt3rd@gmail.com>
2023-11-12 15:16:42 +1300

    add go to background and loop

--- src/main.c
+++ src/main.c
@@ -6,9 +6,19 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int main( int argc, char *argv[] )
 {
     printf( "\nStarting Hamware version " VERSION_STRING "\n" );
+    if ( daemonize( -1, 1 ) != 0 )
+        exit( -1 );
+
+    while ( 1 )
+    {
+        printf( "running...\n" );
+        sleep( 1 );
+    }
+
     return 0;
 }
